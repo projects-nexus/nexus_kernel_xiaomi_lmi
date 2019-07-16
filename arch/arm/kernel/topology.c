5228bc994fbcf7724c0a351d35cfbe77b6e8209d
mydongistiny <jaysonedson@gmail.com>
2019-07-15 23:06:31 -0700

    arm/arm64: topology: Remove clock-frequency pr_err
    We all know it's missing. No need to tell us again in demsg.
    
    Change-Id: Ic0a66ad7775cbeb5640879add2fb04ca845d8fe3
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- arch/arm/kernel/topology.c
+++ arch/arm/kernel/topology.c
@@ -127,7 +127,6 @@ static void __init parse_dt_topology(void)
 
 		rate = of_get_property(cn, "clock-frequency", &len);
 		if (!rate || len != 4) {
-			pr_err("%pOF missing clock-frequency property\n", cn);
 			continue;
 		}
 
