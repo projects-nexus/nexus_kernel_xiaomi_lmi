6471a612b77edfa0b21c012e1ff2a9906f54ea83
Sultan Alsawaf <sultan@kerneltoast.com>
2021-01-24 21:39:41 -0800

    drm: Boost DDR bus upon running an atomic ioctl
    In order to reduce jank, request a DDR bus boost whenever an atomic
    ioctl is sent, which is what our display HAL uses to commit frames to
    the display. The boost should be sufficient enough to render the
    display's maximum FPS without any dropped frames when there is no
    significant external source of load.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -30,7 +30,11 @@
 #include <drm/drm_atomic.h>
 #include <drm/drm_mode.h>
 #include <drm/drm_print.h>
+<<<<<<< HEAD
 #include <drm/drm_writeback.h>
+=======
+#include <linux/devfreq_boost.h>
+>>>>>>> 5d2c7d9959c05... drm: Boost DDR bus upon running an atomic ioctl
 #include <linux/sync_file.h>
 #include <linux/devfreq_boost.h>
 
@@ -2577,12 +2581,19 @@ int drm_mode_atomic_ioctl(struct drm_device *dev,
 			(arg->flags & DRM_MODE_PAGE_FLIP_EVENT))
 		return -EINVAL;
 
+<<<<<<< HEAD
 	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY)) {
 		devfreq_boost_kick(DEVFREQ_MSM_CPU_LLCCBW);
 		devfreq_boost_kick(DEVFREQ_MSM_LLCCBW_DDR);
 	}
 
 	drm_modeset_acquire_init(&ctx, DRM_MODESET_ACQUIRE_INTERRUPTIBLE);
+=======
+	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY))
+		devfreq_boost_kick(DEVFREQ_CPU_LLCC_DDR_BW);
+
+	drm_modeset_acquire_init(&ctx, 0);
+>>>>>>> 5d2c7d9959c05... drm: Boost DDR bus upon running an atomic ioctl
 
 	state = drm_atomic_state_alloc(dev);
 	if (!state)
