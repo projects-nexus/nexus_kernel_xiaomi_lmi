8249de71e242655707dcdb861ae3c429a51a7516
NotZeetaa <rodrigo2005contente@gmail.com>
2021-06-10 10:38:01 +0100

    Revert "locking/rwsem: Make rwsem->owner an atomic_long_t"
    This reverts commit 6c7bbef4ed342adced822045f6cf916d13f42673.

--- include/linux/rwsem.h
+++ include/linux/rwsem.h
@@ -30,12 +30,10 @@ struct rw_semaphore {
 #ifdef CONFIG_RWSEM_SPIN_ON_OWNER
 	struct optimistic_spin_queue osq; /* spinner MCS lock */
 	/*
-	 * Write owner or one of the read owners as well flags regarding
-	 * the current state of the rwsem. Can be used as a speculative
-	 * check to see if the write owner is running on the cpu.
+	 * Write owner. Used as a speculative check to see
+	 * if the owner is running on the cpu.
 	 */
-	atomic_long_t owner;
-	struct optimistic_spin_queue osq; /* spinner MCS lock */
+	struct task_struct *owner;
 #endif
 #ifdef CONFIG_DEBUG_LOCK_ALLOC
 	struct lockdep_map	dep_map;
@@ -50,7 +48,7 @@ struct rw_semaphore {
  * Setting all bits of the owner field except bit 0 will indicate
  * that the rwsem is writer-owned with an unknown owner.
  */
-#define RWSEM_OWNER_UNKNOWN	(-2L)
+#define RWSEM_OWNER_UNKNOWN	((struct task_struct *)-2L)
 
 /* In all implementations count != 0 means locked */
 static inline int rwsem_is_locked(struct rw_semaphore *sem)
@@ -70,7 +68,7 @@ static inline int rwsem_is_locked(struct rw_semaphore *sem)
 #endif
 
 #ifdef CONFIG_RWSEM_SPIN_ON_OWNER
-#define __RWSEM_OPT_INIT(lockname) , .osq = OSQ_LOCK_UNLOCKED, .owner = ATOMIC_LONG_INIT(0)
+#define __RWSEM_OPT_INIT(lockname) , .osq = OSQ_LOCK_UNLOCKED, .owner = NULL
 #else
 #define __RWSEM_OPT_INIT(lockname)
 #endif
