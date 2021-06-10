3a7aefb52d5e7c00985a264b2fb88bcd5990687a
NotZeetaa <rodrigo2005contente@gmail.com>
2021-06-10 10:45:07 +0100

    Revert "locking/rwsem: Prevent unneeded warning during locking selftest"
    This reverts commit ab8e4f40f87136da60bb9369b9f17e918595aba7.

--- kernel/locking/rwsem.h
+++ kernel/locking/rwsem.h
@@ -28,8 +28,7 @@
 
 #ifdef CONFIG_DEBUG_RWSEMS
 # define DEBUG_RWSEMS_WARN_ON(c, sem)	do {			\
-	if (!debug_locks_silent &&				\
-	    WARN_ONCE(c, "DEBUG_RWSEMS_WARN_ON(%s): count = 0x%lx, owner = 0x%lx, curr 0x%lx, list %sempty\n",\
+	if (WARN_ONCE(c, "DEBUG_RWSEMS_WARN_ON(%s): count = 0x%lx, owner = 0x%lx, curr 0x%lx, list %sempty\n",\
 		#c, atomic_long_read(&(sem)->count),		\
 		(long)((sem)->owner), (long)current,		\
 		list_empty(&(sem)->wait_list) ? "" : "not "))	\
