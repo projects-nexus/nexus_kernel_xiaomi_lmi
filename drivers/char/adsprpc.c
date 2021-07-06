78d791270018be06260027be539436cc2f8560ea
Miguel de Dios <migueldedios@google.com>
2021-07-06 14:26:28 +0900

    drivers: char: adsprpc: Specify sync probe for fastrpc
    Bug: 115776306
    Bug: 77146523
    Signed-off-by: Miguel de Dios <migueldedios@google.com>
    Change-Id: Ibcf8405a56034be2a43c1fd03068520b220b81a4

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -5111,6 +5111,7 @@ static struct platform_driver fastrpc_driver = {
 		.name = "fastrpc",
 		.of_match_table = fastrpc_match_table,
 		.suppress_bind_attrs = true,
+		.probe_type = PROBE_FORCE_SYNCHRONOUS,
 	},
 };
 
