4ef01083e8710c4c2be870a4db43b56a1e04659e
NotZeetaa <rodrigo2005contente@gmail.com>
2021-08-07 15:44:29 +0100

    boeffla_wl_blocker: Block smp2p-sleepstate
    Fix deep sleep issues
    
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- drivers/base/power/boeffla_wl_blocker.h
+++ drivers/base/power/boeffla_wl_blocker.h
@@ -16,7 +16,7 @@
 
 #define BOEFFLA_WL_BLOCKER_VERSION	"1.1.0"
 
-#define LIST_WL_DEFAULT				"qcom_rx_wakelock;wlan;wlan_wow_wl;wlan_extscan_wl;netmgr_wl;NETLINK;IPA_WS;[timerfd];wlan_ipa;wlan_pno_wl;wcnss_filter_lock;DIAG_WS"
+#define LIST_WL_DEFAULT				"qcom_rx_wakelock;wlan;wlan_wow_wl;wlan_extscan_wl;netmgr_wl;NETLINK;IPA_WS;[timerfd];wlan_ipa;wlan_pno_wl;wcnss_filter_lock;DIAG_WS;smp2p-sleepstate"
 
 #define LENGTH_LIST_WL				400
 #define LENGTH_LIST_WL_DEFAULT		296
