1cbc836faf18f220afd933fa58b30fe2a702491c
Jebaitedneko <jebaitedneko@gmail.com>
2021-08-09 08:58:42 +0100

    vidc: don't spam when video instance changes

--- drivers/media/platform/msm/vidc_3x/msm_vidc.c
+++ drivers/media/platform/msm/vidc_3x/msm_vidc.c
@@ -1259,8 +1259,6 @@ void *msm_vidc_open(int core_id, int session_type)
 		goto err_invalid_core;
 	}
 
-	pr_info(VIDC_DBG_TAG "Opening video instance: %pK, %d\n",
-		VIDC_MSG_PRIO2STRING(VIDC_INFO), inst, session_type);
 	mutex_init(&inst->sync_lock);
 	mutex_init(&inst->bufq[CAPTURE_PORT].lock);
 	mutex_init(&inst->bufq[OUTPUT_PORT].lock);
@@ -1434,8 +1432,6 @@ int msm_vidc_destroy(struct msm_vidc_inst *inst)
 	mutex_destroy(&inst->lock);
 
 	msm_vidc_debugfs_deinit_inst(inst);
-	pr_info(VIDC_DBG_TAG "Closed video instance: %pK\n",
-			VIDC_MSG_PRIO2STRING(VIDC_INFO), inst);
 	kfree(inst);
 	return 0;
 }
