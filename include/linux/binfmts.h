a19f1b0f598851c520d694387fb2ad4efdf2e721
NotZeetaa <rodrigo2005contente@gmail.com>
2021-08-11 21:42:03 +0100

    Revert "include: linux: Remove power@1.3-servi & NodeLooperThrea"
    This reverts commit 6a16ec36a4b93bb8d9d58b513faaae7c94ea6ed7.

--- include/linux/binfmts.h
+++ include/linux/binfmts.h
@@ -158,8 +158,9 @@ static inline bool task_is_booster(struct task_struct *tsk)
 	char comm[sizeof(tsk->comm)];
 
 	get_task_comm(comm, tsk);
-	return !strcmp(comm, "init") ||
+	return !strcmp(comm, "init") || !strcmp(comm, "NodeLooperThrea") ||
 	       !strcmp(comm, "power@1.2-servi") ||
+	       !strcmp(comm, "power@1.3-servi") ||
 	       !strcmp(comm, "perf@1.0-servic") ||
 	       !strcmp(comm, "perf@2.0-servic") ||
 	       !strcmp(comm, "init.qcom.post_");
