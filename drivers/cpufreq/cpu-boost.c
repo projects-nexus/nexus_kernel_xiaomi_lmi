0d9707b17adca3ee5048b2ddf2c3c6e74b019f3b
NotZeetaa <rodrigo2005contente@gmail.com>
2021-11-07 10:43:41 +0000

    Revert "cpufreq: cpu-boost: set interval between consecutive boosts"
    This reverts commit 3af5bba80fd24517e51c83fb17eb102ac6ffbc28.

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -77,6 +77,8 @@ static bool sched_boost_active;
 static struct delayed_work input_boost_rem;
 static u64 last_input_time;
 
+#define MIN_INPUT_INTERVAL (70 * USEC_PER_MSEC)
+
 static ssize_t store_input_boost_freq(struct kobject *kobj,
 				      struct kobj_attribute *attr,
 				      const char *buf, size_t count)
@@ -371,7 +373,7 @@ static void cpuboost_input_event(struct input_handle *handle,
 		return;
 
 	now = ktime_to_us(ktime_get());
-	if ((now - last_input_time) < (input_boost_ms * USEC_PER_MSEC))
+	if (now - last_input_time < MIN_INPUT_INTERVAL)
 		return;
 
 	if (work_pending(&input_boost_work))
@@ -392,7 +394,7 @@ void touch_irq_boost(void)
 		return;
 
 	now = ktime_to_us(ktime_get());
-	if ((now - last_input_time) < (input_boost_ms * USEC_PER_MSEC))
+	if (now - last_input_time < MIN_INPUT_INTERVAL)
 		return;
 
 	if (work_pending(&input_boost_work))
